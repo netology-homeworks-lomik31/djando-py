e2e03c47fcaf1954117ff2e111107253b1264f07
lomik31 <lomik31@codead.dev>
2023-08-28 21:46:21 +0300

    удалены комментарии

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from calculator.views import gen_recipe
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
     path("receipets/<dish>/", gen_recipe)
 ]
