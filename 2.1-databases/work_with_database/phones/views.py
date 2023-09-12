c4117005d4f660646cdea536a5a750add9816524
lomik31 <lomik31@codead.dev>
2023-09-12 15:52:42 +0300

    добавлена возможность сортировки каталога

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render, redirect
 from .models import Phone
 
 
-def index(request):
+def index(req):
     return redirect("catalog")
 
 
-def show_catalog(request):
+def show_catalog(req):
     template = "catalog.html"
-    context = {"phones": Phone.objects.all()}
-    return render(request, template, context)
+    sort = req.GET.get("sort", "id")
+    if sort == "name": pass
+    elif sort == "max_price": sort = "-price"
+    elif sort == "min_price": sort = "price"
+    else: sort = "id"
+    context = {"phones": Phone.objects.all().order_by(sort)}
+    return render(req, template, context)
 
 
-def show_product(request, slug):
+def show_product(req, slug):
     template = "product.html"
     phone = Phone.objects.filter(slug=slug)
     context = {"phone": phone[0]}
-    return render(request, template, context)
+    return render(req, template, context)
