fab9c592e3df3c37f7a7b2e767fd7ccb8e9bfa03
lomik31 <lomik31@codead.dev>
2023-09-12 14:54:54 +0300

    создана модель Phone

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists  = models.BooleanField()
+    slug = models.SlugField()
