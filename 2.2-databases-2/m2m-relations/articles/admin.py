f086bb4265fa34321e6b2bcb76f07c7f8550b12c
lomik31 <lomik31@codead.dev>
2023-09-21 02:14:18 +0300

    настроена админ-панель

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        if (len(list(filter(lambda i: len(i.cleaned_data) > 0, self.forms))) == 0): raise ValidationError("Необходимо добавить хотя бы один тег")
+        isMainCount = 0
+        for form in self.forms:
+            if (form.cleaned_data.get("is_main") == True): isMainCount += 1
+        if (isMainCount > 1): raise ValidationError("Главный тег может быть только один")
+        if (isMainCount < 1): raise ValidationError("Необходимо назначить главный тег")
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
+class ObjectAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
