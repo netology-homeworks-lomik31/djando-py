7eded3093f2ec0052c618bec14eb7454d6859726
lomik31 <lomik31@codead.dev>
2023-10-10 21:45:14 +0300

    added database models

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.timezone import now
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=100)
+
+class Measurement(models.Model):
+    name = models.ForeignKey(Sensor, on_delete=models.CASCADE, blank=False)
+    value = models.IntegerField(blank=False)
+    date = models.DateTimeField(default=now, blank=False)
